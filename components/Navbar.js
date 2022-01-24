97a2b700e705c5973430997ad36d87b73cc8195a
Debabrata Mondal <debabrata.js@protonmail.com>
2022-01-24 10:46:50 +0530

    update navbar state

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,32 @@ import { supabase } from "../utils/supabaseClient"
 import Link from "next/link"
 
 import styles from "../styles/navbar.module.css"
+import {useEffect, useState} from "react"
 
 export default function Navbar() {
+
+    const [session, setSession] = useState(supabase.auth.session() ? supabase.auth.session() : null)
+
+    useEffect(() => {
+        setSession(supabase.auth.session())
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+    }, [])
+
     return (
         <div className={styles.nav}>
             <Link href="/">
                 <h1>TaskBase</h1>
             </Link>
-            <button
-                onClick={() => supabase.auth.signOut()}
-            >
-                Sign out
-            </button>
+            {session && (
+                <button
+                    onClick={() => supabase.auth.signOut()}
+                >
+                    Sign out
+                </button>
+            )}
         </div>
     )
 }
